dbdafdacc37471d546422573676f51d5f30b2e0b
lupeng <1535933394@qq.com>
2018-07-19 01:26:23 +0000

    Add captcha

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -3,7 +3,6 @@
         <p class="pull-left">
             由 <a href="#" target="_blank">Summer</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
         </p>
-
         <p class="pull-right"><a href="#">联系我们</a></p>
     </div>
 </footer>
--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,7 +1,6 @@
 <nav class="navbar navbar-default navbar-static-top">
     <div class="container">
         <div class="navbar-header">
-
             <!-- Collapsed Hamburger -->
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
                 <span class="sr-only">Toggle Navigation</span>
@@ -19,9 +18,7 @@
         <div class="collapse navbar-collapse" id="app-navbar-collapse">
             <!-- Left Side Of Navbar -->
             <ul class="nav navbar-nav">
-
             </ul>
-
             <!-- Right Side Of Navbar -->
             <ul class="nav navbar-nav navbar-right">
                 <!-- Authentication Links -->
