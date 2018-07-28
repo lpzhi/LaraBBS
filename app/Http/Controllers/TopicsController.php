ee880fdf713217b63f39a60b03c5a2f147093ab4
lupeng <1535933394@qq.com>
2018-07-28 09:05:43 +0000

    Topic index page

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -16,7 +16,8 @@ class TopicsController extends Controller
 
 	public function index()
 	{
-		$topics = Topic::paginate();
+		$topics = Topic::with('user', 'category')->paginate(30);
+
 		return view('topics.index', compact('topics'));
 	}
 
--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -2,7 +2,21 @@
 
 namespace App\Models;
 
+use App\Models\Category;
+use App\Models\User;
+
 class Topic extends Model
 {
     protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class,'category_id','id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class,'user_id','id');
+    }
+
 }
