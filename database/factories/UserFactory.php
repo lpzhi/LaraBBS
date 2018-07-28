2f36d5282d570f4ad221057f7a0557d26aab137a
lupeng <1535933394@qq.com>
2018-07-28 07:00:54 +0000

    Seeding topics and users data

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -15,11 +15,15 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Models\User::class, function (Faker $faker) {
     static $password;
+    $now = Carbon::now()->toDateTimeString();
 
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
+        'password' => $password ?: $password = bcrypt('password'),
         'remember_token' => str_random(10),
+        'introduction' => $faker->sentence(),
+        'created_at' => $now,
+        'updated_at' => $now,
     ];
 });
